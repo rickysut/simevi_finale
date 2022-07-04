7c35ee40b659c1b154ce801da823ae971f568136
sultanjawer <108628095+sultanjawer@users.noreply.github.com>
2022-07-04 22:52:29 +0700

    Add files via upload

---
+++ app/Resources/Admin/RincianOutputResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RincianOutputResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
